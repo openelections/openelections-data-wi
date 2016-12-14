4817fa0fad913b9216c20368024a272ae05651f1
Nick Davies <nbdavies@gmail.com>
2016-12-13 20:02:24 -0600

    Make party an optional criteria to test specific output lines

--- tests/features/steps/steps.py
+++ tests/features/steps/steps.py
@@ -17,25 +17,29 @@ def step_impl(context):
     else:
       raise AssertionError("%s not found" % path)
 
-@when('I search for candidate {candidate} running for {office} in the {ward} in {county}')
-def step_impl(context,candidate,office,ward,county):
+@when('I search for {party} party candidate {candidate} running for {office} in the {ward} in {county}')
+def step_impl(context, party, candidate, office, ward, county):
     context.passes = False
-
-    print ("Expecting %s %s %s %s" % (candidate.title(), office.title(), ward.title(), county.title()))
+    field_names = ['party', 'candidate', 'ward', 'office', 'county']
+    if party == '<party>':  # No party was specified in the test, don't check party
+      field_names.remove('party') 
+    expected_values = [locals()[field_name] for field_name in field_names]
+    expected_values = map(unicode.title, expected_values)
+    print ("Expecting " + ", ".join(expected_values))
 
     election_data = csv.DictReader(open(context.path))
     for row in election_data:
 ###   Generates too many lines in log
-###      print ("Found %s %s %s %s %s %s" % (row['candidate'], row['office'], row['ward'], row['county'], row['votes'], row['total votes']))
-      if (row['candidate'] == candidate.title() and row['ward'] == ward.title() and row['office'] == office.title()  and row['county'] == county.title()):
+###      print ("Found %s %s %s %s %s %s" % (row['candidate'], row['office'], row['ward'], row['county'], row['votes'], row['total votes'])) 
+      actual_values = [row[field_name] for field_name in field_names]
+      if actual_values == expected_values:
+        if context.passes:
+          raise AssertionError("More than one row found with expected values")
         context.passes = True
         context.votes = row['votes']
         context.total = row['total votes']
-    pass
-    if (context.passes):
-      pass
-    else:
-      raise AssertionError("Record not found for %s" % candidate.title())
+    if not context.passes:
+      raise AssertionError("No record found with expected values")
 
 @then('I should see {votes} out of {total}')
 def step_impl(context,votes,total):
