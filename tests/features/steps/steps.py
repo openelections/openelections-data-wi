287553d260f805097a5972084956e3437cf39270
David Post <post@pobox.com>
2017-01-05 23:35:11 -0600

    Add comments about title-casing test data and results (this should not be necessary)

--- tests/features/steps/steps.py
+++ tests/features/steps/steps.py
@@ -27,12 +27,14 @@ def step_impl(context, party, candidate, office, ward, county):
     if party == '<party>':  # No party was specified in the test, don't check party
       field_names.remove('party') 
     expected_values = [locals()[field_name] for field_name in field_names]
+    ## Title-case for consistency -- remove this and edit test data instead?
     expected_values = map(unicode.title, expected_values)
     print ("Expecting " + ", ".join(expected_values))
 
     election_data = unicodecsv.DictReader(open(context.path))
     for row in election_data:
       actual_values = [row[field_name] for field_name in field_names]
+      ## Title-case for consistency -- should not be necessary (see above)
       actual_values = map(unicode.title, actual_values)
       if actual_values == expected_values:
         if context.passes:
