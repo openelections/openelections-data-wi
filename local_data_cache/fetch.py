eb9a7ef1c108006cf5b859c681de0df1881d7ec2
David Post <post@pobox.com>
2017-10-10 19:31:17 -0500

    fetch.py -- added cmd-line option to download files for a single id

--- local_data_cache/fetch.py
+++ local_data_cache/fetch.py
@@ -1,7 +1,11 @@
 import requests
 import json
+import sys
 
-WIOpenElectionsAPI = "http://openelections.net/api/v1/election/?format=json&limit=0&state__postal=WI"
+
+url_base = "http://openelections.net/api/v1/election/"
+url_query = "?format=json&limit=0&state__postal=WI"
+
 
 def save_file(url, filename):
     url = url.strip()
@@ -12,18 +16,33 @@ def save_file(url, filename):
     else:
       print "Could not download file %s, status code %s" % (url, r.status_code)
 
-def update_cache(url):
-  url = url.strip()
-  r = requests.get(url)
-  if r.status_code == 200:
-    parsed = json.loads(r.content)['objects']
-    for i in parsed:
-      if i['direct_links'] == []:
-        print "no results for id: ",i['id']
-      else:
-        for download_url in i['direct_links']:
-          filename = "data/%s" % download_url.split('/')[-1]
-          print "Downloading %s" % filename
-          save_file(download_url, filename)
 
-update_cache(WIOpenElectionsAPI)
+def update_cache(url):
+    print url
+    url = url.strip()
+    r = requests.get(url)
+    if r.status_code == 200:
+        parsed = json.loads(r.content)['objects']
+        for election in parsed:
+            if election['direct_links'] == []:
+                print "no results for id: ", election['id']
+            else:
+                for download_url in election['direct_links']:
+                    filename = "data/%s" % download_url.split('/')[-1]
+                    print "Downloading %s" % filename
+                    save_file(download_url, filename)
+
+
+# Running without args, download all files.
+# With args, get files for the ids listed as args.
+if __name__ == '__main__':
+    args = sys.argv[1:]
+    if args:
+        if all(map(str.isdigit, args)):
+            for arg in args:
+                update_cache(url_base + arg + '/' + url_query)
+        else:
+            print 'Args must be positive integers (election ids)'
+    else:
+        update_cache(url_base + url_query)
+
